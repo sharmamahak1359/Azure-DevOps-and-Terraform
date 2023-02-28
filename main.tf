00f3c9bbb2d2ebb110a87f06aad0492389a61cc7
Mahak Sharma <sharmamahak1359@gmail.com>
2023-02-28 11:06:57 +0530

    commit

--- main.tf
+++ main.tf
@@ -48,10 +48,10 @@ resource "azurerm_app_service" "appservice" {
     location = azurerm_resource_group.resourcegroup.location
     resource_group_name = azurerm_resource_group.resourcegroup.name
     app_service_plan_id = azurerm_app_service_plan.serviceplan.id
-  //site_config {
-    //dotnet_framework_version = "v5.0"
-    //always_on                = true
-  //}
+  site_config {
+    dotnet_framework_version = "v5.0"
+    always_on                = true
+  }
 
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
